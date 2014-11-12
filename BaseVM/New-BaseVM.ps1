9f8e40edeff8f97687e72439f639a6d9dd63ca51
Craig Martin <craig.martin@edgile.com>
2014-11-12 11:17:24 -0800

    Adding DSC test scripts for SQL and SP

---
+++ BaseVM/New-BaseVM.ps1
@@ -0,0 +1,70 @@
+﻿
+Set-Location $HOME
+
+$SwitchName       = Get-VMSwitch -SwitchType External | Select-Object -expand Name -First 1
+$vmName           = "CraigmDev1111-4"
+$vhdFilePath      = "S:\Hyper-V\Virtual Hard Disks\$vmName.vhdx"
+$isoFolderPath    = "S:\ISO"
+$StartUpMemoryMB  = 4096
+$ProcessorCount   = 4
+$UnattendFilePath = "$HOME\$vmName.xml"
+
+[xml]$UnattendFile = @'
+<?xml version='1.0' encoding='utf-8'?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+  <settings pass="specialize">
+    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <ComputerName>{0}</ComputerName>
+      <TimeZone>Pacific Standard Time</TimeZone>
+    </component>
+  </settings>
+  <settings pass="oobeSystem">
+    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+      <OOBE>
+        <HideEULAPage>true</HideEULAPage>
+        <SkipMachineOOBE>true</SkipMachineOOBE>
+        <SkipUserOOBE>true</SkipUserOOBE>
+        <ProtectYourPC>3</ProtectYourPC>
+        <NetworkLocation>Work</NetworkLocation>
+      </OOBE>
+      <UserAccounts>
+        <AdministratorPassword>
+          <Value>PA$$w0rd2014</Value>
+          <PlainText>true</PlainText>
+        </AdministratorPassword>
+      </UserAccounts>
+    </component>
+  </settings>
+</unattend>
+'@ -F $vmName
+
+$UnattendFile.Save($UnattendFilePath)
+
+###
+### Create the Virtual Disk from the ISO
+###
+$convertwindowsimageParameters = @{
+SourcePath          = (Join-Path $isoFolderPath en_windows_server_2012_r2_x64_dvd_2707946.iso)
+UnattendPath        = $UnattendFilePath
+Edition             = 'ServerDataCenter'
+VHDPath             = $vhdFilePath
+VHDPartitionStyle   = 'GPT'
+VHDFormat           = 'VHDX'
+RemoteDesktopEnable = $true
+}
+& 'D:\Users\v-crmart\Desktop\VM Automation\convertwindowsimage.codeplex.com\Convert-WindowsImage.ps1' @convertwindowsimageParameters     
+
+
+### Create the VM
+New-VM -Name $vmName -VHDPath $vhdFilePath -SwitchName $SwitchName -MemoryStartupBytes ($StartUpMemoryMB*1MB) -Generation 2 -BootDevice VHD 
+Set-VM -Name $vmName -ProcessorCount $ProcessorCount
+
+### Start the VM
+Start-VM -Name $vmName          
+
+### Add a DVD Drive
+Add-VMDvdDrive -VMName $vmName
+
+### Add the new VM to the TrustedHosts for this VM Host
+set-item WSMan:\localhost\Client\TrustedHosts –value $vmName
+
---
+++ Test-SqlInstall/Test-SqlInstall.ps1
@@ -0,0 +1,54 @@
+﻿$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName               = (hostname)
+            CertificateFile        = "c:\Temp\Certificates\craigweb.corp.microsoft.com.cer"
+         }
+    )
+}
+
+Configuration SqlInstall
+{
+    
+    Import-DscResource -ModuleName cSqlPs
+
+    $saCred                 = New-Object System.Management.Automation.PSCredential sa, (ConvertTo-SecureString 'PA$$w0rd2014' -AsPlainText -Force)
+
+    node $AllNodes.NodeName
+    {
+        LocalConfigurationManager 
+        { 
+             CertificateId = "13AA23245848B50C787D56C15595400213BA2E15" 
+             RebootNodeIfNeeded = 'true'
+             ConfigurationModeFrequencyMins = '15'
+        } 
+
+        #region SQL Server Install
+        cSqlServerInstall InstallSQLServer
+        {
+            InstanceName = "MSSQLSERVER"
+            SourcePath = "C:\Temp\SQL"           
+            SqlAdministratorCredential = $saCred
+            PID = "7TFR8-FMK2W-MWDXP-J224F-JTR8F"
+            SysAdminAccounts = 'administrator'
+            #DependsOn = "[File]SQLServerIso"
+        }
+
+        ## Start the sql Agent service
+        Service StartSQLAgent
+        {
+            Name = "SQLSERVERAGENT"
+            StartupType = "Automatic"
+            State = "Running"
+            DependsOn = "[cSqlServerInstall]InstallSQLServer"
+
+        }   
+        #endregion  
+    }
+}
+
+SqlInstall -ConfigurationData $ConfigurationData -OutputPath c:\temp\SqlInstall
+
+Set-DscLocalConfigurationManager -Path c:\temp\SqlInstall
+
+Start-DscConfiguration -Verbose -Wait -Path c:\temp\SqlInstall
