9c7939612a08a836b176b6141a771792515e936b
Craig Martin <craig.martin@edgile.com>
2014-11-12 17:00:19 -0800

    Adding test scripts for Domain joining, and SP config

---
+++ Test-SharePointConfigForFim/Test-SharePointConfigForFim.ps1
@@ -0,0 +1,132 @@
+﻿#TODO - fold both these resources into one
+#ISSUE - it'll be difficult to set this once it is created, consider that out of scope just to get done, throw
+
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName               = (hostname)
+            CertificateFile        = "c:\Temp\Certificates\craigweb.corp.microsoft.com.cer"
+         }
+    )
+}
+
+configuration SharePointConfigForFim
+{
+
+    Import-DscResource -ModuleName cSqlPs
+    Import-DsCResource -ModuleName xPendingReboot
+    Import-DsCResource -ModuleName xComputerManagement
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    $saCred                 = New-Object System.Management.Automation.PSCredential sa, (ConvertTo-SecureString 'PA$$w0rd2014' -AsPlainText -Force)
+    $domainCredential       = New-Object System.Management.Automation.PSCredential REDMOND\cmfim1, (ConvertTo-SecureString '12thManFactor%' -AsPlainText -Force)
+
+    node $AllNodes.NodeName
+    {
+        LocalConfigurationManager 
+        { 
+             CertificateId = "13AA23245848B50C787D56C15595400213BA2E15" 
+             RebootNodeIfNeeded = 'true'
+             ConfigurationModeFrequencyMins = '15'
+        } 
+
+        Script SetupSharepointAdmin
+        {
+            #DependsOn = "[Package]InstallSharePointFoundation"
+                 
+            GetScript = {Return "SetupSharepointAdmin"}
+            TestScript = {
+                add-pssnapin Microsoft.SharePoint.PowerShell
+                if (Get-SPServer)
+                {
+                    Write-Verbose "SPServer Found, returning True"
+                    return $true                     
+                }
+                else
+                {
+                    Write-Verbose "SPServer Not Found, returning False"
+                    return $false
+                }
+            }
+            SetScript = {
+                add-pssnapin Microsoft.SharePoint.PowerShell
+
+                $databaseServer = (hostname)
+                $configDatabase = "FIMSP_Config" 
+                $adminContentDB = "FIMSP_Content_Admin" 
+
+                $adminCredential = New-Object System.Management.Automation.PSCredential "$(hostname)\administrator",(ConvertTo-SecureString 'PA$$w0rd2014' -AsPlainText -Force)
+
+                New-SPConfigurationDatabase -DatabaseServer $databaseServer -DatabaseName $configDatabase -AdministrationContentDatabaseName $adminContentDB -Passphrase $adminCredential.Password -FarmCredentials $adminCredential 
+                Write-Verbose "ACLing SharePoint Resources..."
+                Initialize-SPResourceSecurity 
+
+                Write-Verbose "Installing Services ..." 
+                Install-SPService   
+
+                Write-Verbose "Installing Features..." 
+                Install-SPFeature -AllExistingFeatures 
+
+                Write-Verbose "Creating Central Administration..."              
+                New-SPCentralAdministration -Port 8080 -WindowsAuthProvider NTLM 
+
+                Write-Verbose "Installing Application Content..." 
+                Install-SPApplicationContent 
+
+                #REBOOT JUST BECUASE
+                #$global:DSCMachineStatus = 1    
+            }
+        }
+
+        Script SetupSharePointSite
+        {
+            
+            GetScript = {Return "SetupSharePointSite"}
+            TestScript = {
+                add-pssnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+                $contentService = [Microsoft.SharePoint.Administration.SPWebService]::ContentService
+                $viewStateOnServer = $contentService.ViewStateOnServer
+
+                $spSite = SpSite("Http://localhost") -ErrorAction SilentlyContinue
+                $allowSelfServiceUpgrade = $spSite.AllowSelfServiceUpgrade            
+
+                $fimSpWebApplication = Get-SPWebApplication -Identity 'FIM SharePoint Web Application' -ErrorAction SilentlyContinue
+
+                if ($viewStateOnServer -eq $false -and $allowSelfServiceUpgrade -eq $false -and $fimSpWebApplication)
+                {
+                    return $true
+                }
+                else
+                {
+                    return $false
+                }
+            }
+            SetScript = {
+                add-pssnapin Microsoft.SharePoint.PowerShell
+                
+                Write-Verbose "Creating SharePoint Web Application" 
+                New-SpWebApplication -Name "FIM SharePoint Web Application" -ApplicationPool "FIMAppPool" -AuthenticationMethod NTLM -ApplicationPoolAccount (Get-SPManagedAccount -Identity "$(hostname)\administrator") -Port 80 -URL http://localhost
+                
+                Write-Verbose "Creating SharePoint root Site Collection..." 
+                New-SPSite -Url "http://localhost" -OwnerAlias "$(hostname)\administrator" -Template "STS#1" -CompatibilityLevel 14
+                
+                Write-Verbose "Configuring View State..."
+                $contentService = [Microsoft.SharePoint.Administration.SPWebService]::ContentService
+                $contentService.ViewStateOnServer = $false
+                $contentService.Update()
+
+                Write-Verbose "Disabling self service upgrade..."
+                $spSite = SpSite("http://localhost")
+                $spSite.AllowSelfServiceUpgrade = $false
+                                                           
+            }
+            DependsOn = "[Script]SetupSharepointAdmin"
+        }
+    }
+}
+
+SharePointConfigForFim -ConfigurationData $ConfigurationData -OutputPath c:\temp\SharePointConfigForFim
+
+Set-DscLocalConfigurationManager -Path c:\temp\SharePointConfigForFim
+Get-DscLocalConfigurationManager
+Start-DscConfiguration -Verbose -Wait -Path c:\temp\SharePointConfigForFim 
---
+++ Test-SqlAndSharePointDomainJoined/Test-SqlAndSharePointDomainJoined.ps1
@@ -0,0 +1,237 @@
+﻿$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName               = (hostname)
+            CertificateFile        = "c:\Temp\Certificates\craigweb.corp.microsoft.com.cer"
+         }
+    )
+}
+
+configuration SqlAndSharePointDomainJoined
+{
+
+    Import-DscResource -ModuleName cSqlPs
+    Import-DsCResource -ModuleName xPendingReboot
+    Import-DsCResource -ModuleName xComputerManagement
+
+    $saCred                 = New-Object System.Management.Automation.PSCredential sa, (ConvertTo-SecureString 'PA$$w0rd2014' -AsPlainText -Force)
+    $domainCredential       = New-Object System.Management.Automation.PSCredential REDMOND\cmfim1, (ConvertTo-SecureString '12thM@nFactor%' -AsPlainText -Force)
+
+    node $AllNodes.NodeName
+    {
+        LocalConfigurationManager 
+        { 
+             CertificateId = "13AA23245848B50C787D56C15595400213BA2E15" 
+             RebootNodeIfNeeded = 'true'
+             ConfigurationModeFrequencyMins = '15'
+        } 
+
+        #region Join the Domain
+        xComputer JoinDomain
+        {
+            Name             = $Node.NodeName
+            DomainName       = 'redmond.corp.microsoft.com'
+            Credential       = $domainCredential 
+        }
+
+        xGroup AddCredentialUserToAdmins
+        {
+            GroupName        = "Administrators"
+            Ensure           = "Present"
+            MembersToInclude = 'redmond\v-crmart'
+            Credential       = $domainCredential
+            DependsOn        = "[xComputer]JoinDomain"
+        }
+        #endregion
+
+        #region Windows Features
+        WindowsFeature  WindowsIdentityFoundation
+        {
+            Ensure = "Present"
+            Name   = "Windows-Identity-Foundation"
+            IncludeAllSubFeature = $true   
+        }
+
+        WindowsFeature NetFramework35Core
+        {
+            Name = "NET-Framework-Core"
+            Ensure = "Present"   
+        }
+ 
+        WindowsFeature NetFramework45Full
+        {
+            Name = "NET-Framework-45-Features"
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        WindowsFeature WebMgmtTools
+        {
+            Ensure = "Present"
+            Name   = "Web-Mgmt-Tools"
+            IncludeAllSubFeature = $true  
+        }
+
+        WindowsFeature WebWebServer
+        {
+            Name = "Web-WebServer"
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        WindowsFeature ApplicationServer
+        {
+            Ensure = "Present"
+            Name   = "Application-Server"
+            IncludeAllSubFeature = $true           
+        }
+ 
+        #endregion
+
+        #region SharePoint Prerequisites
+        Package MicrosoftIdentityExtensions
+        {
+            Ensure    = "Present"  
+            Path      = "C:\Temp\SharePointPreReqs\MicrosoftIdentityExtensions-64.msi"
+            Name      = "Microsoft Identity Extensions"
+            ProductId = "F99F24BF-0B90-463E-9658-3FD2EFC3C992"
+            LogPath   = "c:\temp\MicrosoftIdentityExtensions.log"
+            DependsOn = "[WindowsFeature]WindowsIdentityFoundation"
+        }
+
+        Package MSIPC
+        {
+            Ensure    = "Present"  
+            Path      = "C:\Temp\SharepointPreReqs\setup_msipc_x64.msi"
+            Name      = "Active Directory Rights Management Services Client 2.0"
+            ProductId = "C293A044-3603-4CA3-B8FC-BA3F86F9F169"
+            LogPath   = "c:\temp\setup_msipc_x64.log"
+            DependsOn = "[Package]MicrosoftIdentityExtensions"
+        }
+
+        Package SqlNCli
+        {
+            Ensure    = "Present"   
+            Name      = "Microsoft SQL Server 2012 Native Client "
+            #Path      = "C:\Temp\SharepointPreReqs\sqlncli.msi"
+            #ProductId = "49D665A2-4C2A-476E-9AB8-FCC425F526FC"
+            Path      = "C:\Temp\SQL\1033_ENU_LP\x64\Setup\x64\sqlncli.msi"
+            ProductId = "D411E9C9-CE62-4DBF-9D92-4CB22B750ED5"
+            LogPath   = "c:\temp\sqlncli.log"
+            Arguments = "IACCEPTSQLNCLILICENSETERMS=YES"
+        }
+
+        Package Synchronization
+        {
+            Ensure    = "Present"  
+            Path      = "C:\Temp\SharepointPreReqs\Synchronization.msi"
+            Name      = "Microsoft Sync Framework Runtime v1.0 SP1 (x64)"
+            ProductId = "8438EC02-B8A9-462D-AC72-1B521349C001"
+            LogPath   = "c:\temp\Synchronization.log"
+            DependsOn = "[Package]SqlNCli"
+        }
+
+        Package WcfDataServices5
+        {
+            Ensure    = "Present"  
+            Path      = "C:\Temp\SharePointPreReqs\WCF-5.0\WcfDataServices.exe"
+            Name      = "WCF Data Services 5.0 (OData v3)"
+            ProductId = "ad60454d-9edf-4100-8342-06830732be63"
+            LogPath   = "c:\temp\WcfDataServices.log"
+            Arguments = "/quiet"
+            DependsOn = "[Package]Synchronization"
+        }
+
+        Package WcfDataServices56
+        {
+            Ensure    = "Present"  
+            Path      = "C:\Temp\SharePointPreReqs\WCF-5.6\WcfDataServices.exe"
+            Name      = "WCF Data Services 5.6.0 Runtime"
+            ProductId = "46910786-E4AC-41E4-A4A0-C086EA85242D"
+            LogPath   = "c:\temp\WcfDataServices.log"
+            Arguments = "/quiet"
+            DependsOn = "[Package]Synchronization"
+        }
+        
+        Package AppFabric
+        {
+            <#
+                This package will fail if the PSModulePath contains a quote:
+                    https://social.msdn.microsoft.com/Forums/vstudio/en-US/8cf55972-e441-4107-8c0d-1692ad03213d/error-with-installation-of-appfabric-11-on-psmodulepath?forum=velocity
+                Use this to test: 
+                    [System.Environment]::GetEnvironmentVariable("PSModulePath", [System.EnvironmentVariableTarget]::Machine) -like '*"*'
+            #>
+            Ensure    = "Present"  
+            Path      = "C:\Temp\SharepointPreReqs\WindowsServerAppFabricSetup_x64.exe"
+            Name      = "AppFabric 1.1 for Windows Server"
+            ProductId = "96E70525-4CD1-4920-9C0B-91055C79A962"
+            #LogPath   = "c:\temp\WindowsServerAppFabricSetup_x64.log" # note: this does not work with an EXE and the Package Resource
+            Arguments = "/i cacheclient,cachingService,cacheadmin /gac /l c:\temp\WindowsServerAppFabricSetup_x64.log"
+        }
+
+        Script AppFabricUpdate
+        {
+            GetScript = { Return "AppFabricUpdate" }
+            TestScript = {
+                $regKeyForUpdateKB2671763 = Get-ChildItem "HKLM:\Software\Wow6432Node\Microsoft\Updates\AppFabric 1.1 for Windows Server\KB2671763" -ErrorAction SilentlyContinue
+                if ($regKeyForUpdateKB2671763) {
+                    Write-Verbose "AppFabric Update already installed"
+                    return $true
+                } else {
+                    Write-Verbose "AppFabric Update not installed"
+                    return $false
+                }
+            }
+            SetScript = {               
+                Start-Process -FilePath 'C:\Temp\SharepointPreReqs\AppFabric1.1-RTM-KB2671763-x64-ENU.exe' -ArgumentList '/quiet' -Wait | Write-verbose 
+            }
+            DependsOn = "[Package]AppFabric"
+        }
+        #endregion
+
+        #NOTE - this warning can be safely ignored : "Unable to query CCM_ClientUtilities: Invalid namespace"
+        xPendingReboot BeforeSharePointInstall
+        {
+            Name = "BeforeSharePointInstall"
+        }
+
+        #region SQL Server Install
+        cSqlServerInstall InstallSQLServer
+        {
+            InstanceName = "MSSQLSERVER"
+            SourcePath = "C:\Temp\SQL"           
+            SqlAdministratorCredential = $saCred
+            PID = "7TFR8-FMK2W-MWDXP-J224F-JTR8F"
+            SysAdminAccounts = 'administrator'
+            #DependsOn = "[File]SQLServerIso"
+        }
+
+        ## Start the sql Agent service
+        Service StartSQLAgent
+        {
+            Name = "SQLSERVERAGENT"
+            StartupType = "Automatic"
+            State = "Running"
+            DependsOn = "[cSqlServerInstall]InstallSQLServer"
+
+        }   
+        #endregion  
+
+        Package InstallSharePointFoundation
+        {
+            Ensure             = "Present"
+            Name               = "Microsoft SharePoint Foundation 2013 Core"
+            Path               = "C:\Temp\SharePointFoundation2013\Setup.exe"
+            Arguments          = "/config C:\Temp\SharePointFoundation2013\files\setupsilent\config.xml"
+            ProductID          = "90150000-1014-0000-1000-0000000FF1CE"
+            ReturnCode         = 8
+            DependsOn          = "[Script]AppFabricUpdate","[WindowsFeature]ApplicationServer","[WindowsFeature]WebWebServer","[WindowsFeature]NetFramework45Full","[Package]WcfDataServices5","[Package]WcfDataServices56", "[xPendingReboot]BeforeSharePointInstall", "[cSqlServerInstall]InstallSQLServer"
+        }
+    }
+}
+
+SqlAndSharePointDomainJoined -ConfigurationData $ConfigurationData -OutputPath c:\temp\SqlAndSharePointDomainJoined
+
+Set-DscLocalConfigurationManager -Path c:\temp\SqlAndSharePointDomainJoined
+
+Start-DscConfiguration -Verbose -Wait -Path c:\temp\SqlAndSharePointDomainJoined -Force
