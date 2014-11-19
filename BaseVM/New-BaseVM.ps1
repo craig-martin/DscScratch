81f9e3a8a1c4435a8902e3fe0ac2092cd0bfaef6
Craig Martin <craig.martin@edgile.com>
2014-11-18 16:39:42 -0800

    Organizing into Base and FIM scripts

--- BaseVM/New-BaseVM.ps1
+++ BaseVM/New-BaseVM.ps1
@@ -2,95 +2,40 @@
 Set-Location $HOME
 
 $SwitchName       = Get-VMSwitch -SwitchType External | Select-Object -expand Name -First 1
-$vmName           = "WindowsServer2012R2-Image" #"CraigmDev1118-5"
-$adminCredential  = New-Object System.Management.Automation.PSCredential $vmName\administrator,(ConvertTo-SecureString 'PA$$w0rd2014' -AsPlainText -Force)
-$vhdFilePath      = "S:\Hyper-V\Virtual Hard Disks\$vmName.vhdx"
-$isoFolderPath    = "S:\ISO"
-$StartUpMemoryMB  = 4096
-$ProcessorCount   = 4
-$UnattendFilePath = "$HOME\$vmName.xml"
-
-[xml]$UnattendFile = @'
-<?xml version='1.0' encoding='utf-8'?>
-<unattend xmlns="urn:schemas-microsoft-com:unattend">
-  <settings pass="specialize">
-    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <ComputerName>{0}</ComputerName>
-      <TimeZone>Pacific Standard Time</TimeZone>
-    </component>
-  </settings>
-  <settings pass="oobeSystem">
-    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
-      <OOBE>
-        <HideEULAPage>true</HideEULAPage>
-        <SkipMachineOOBE>true</SkipMachineOOBE>
-        <SkipUserOOBE>true</SkipUserOOBE>
-        <ProtectYourPC>3</ProtectYourPC>
-        <NetworkLocation>Work</NetworkLocation>
-      </OOBE>
-      <UserAccounts>
-        <AdministratorPassword>
-          <Value>PA$$w0rd2014</Value>
-          <PlainText>true</PlainText>
-        </AdministratorPassword>
-      </UserAccounts>
-    </component>
-  </settings>
-</unattend>
-'@ -F $vmName
-
-$UnattendFile.Save($UnattendFilePath)
+$ImageName        = "WindowsServer2012R2-Image"
+$ImageVhdFilePath = "S:\Hyper-V\Virtual Hard Disks\$ImageName.vhdx"
 
 ###
 ### Create the Virtual Disk from the ISO
 ###
 $convertwindowsimageParameters = @{
 SourcePath          = (Join-Path $isoFolderPath en_windows_server_2012_r2_x64_dvd_2707946.iso)
-#UnattendPath        = $UnattendFilePath
 Edition             = 'ServerDataCenter'
-VHDPath             = $vhdFilePath
+VHDPath             = $ImageVhdFilePath
 VHDPartitionStyle   = 'GPT'
 VHDFormat           = 'VHDX'
 RemoteDesktopEnable = $true
 }
 & 'D:\Users\v-crmart\Desktop\VM Automation\convertwindowsimage.codeplex.com\Convert-WindowsImage.ps1' @convertwindowsimageParameters     
 
+### Path to the Base VHD
+$ImageVhdFilePath = 'S:\Hyper-V\Virtual Hard Disks\WindowsServer2012R2-Image.vhdx'
 
-### Install KB2883200 (DSC requires it)
-Mount-WindowsImage -ImagePath $vhdFilePath -Index 1 -Path s:\Temp
+### Add features to the VHD
+Add-WindowsFeature -Vhd $ImageVhdFilePath -IncludeAllSubFeature -Source E:\sources\sxs -Verbose -Name @(
+    'Web-Server'
+    'Web-Mgmt-Tools'
+    'Windows-Identity-Foundation'
+    'NET-Framework-Features'
+    'NET-Framework-45-Features'
+    'Application-Server'
+)
+
+### Install KB2883200 (DSC requires it) 
+Mount-WindowsImage -ImagePath $ImageVhdFilePath -Index 1 -Path s:\Temp
 Add-WindowsPackage -Path s:\Temp -PackagePath S:\Install\Windows8.1-KB2883200-x64.msu -Verbose
+
+### Copy the DSC Resources into the VHD
+dir S:\Install\DscResources\ | copy -Destination 'S:\Temp\Program Files\WindowsPowerShell\Modules' 
 Dismount-WindowsImage -Path s:\Temp -Save 
-
-### Create the VM
-New-VM -Name $vmName -VHDPath $vhdFilePath -SwitchName $SwitchName -MemoryStartupBytes ($StartUpMemoryMB*1MB) -Generation 2 -BootDevice VHD 
-Set-VM -Name $vmName -ProcessorCount $ProcessorCount
-
-### Start the VM
-Start-VM -Name $vmName
-
-### Add a DVD Drive
-Add-VMDvdDrive -VMName $vmName
-
-### Mount the ISO with the FIM OneBox Files
-Set-VMDvdDrive -VMName $vmName -Path S:\ISO\FimOneBoxFiles.iso
-
-### Add the new VM to the TrustedHosts for this VM Host
-set-item WSMan:\localhost\Client\TrustedHosts –value $vmName -Force
-
-### Enable RDP
-Invoke-Command -ComputerName $vmName -Credential $adminCredential -ScriptBlock {
-    #Allow incoming RDP on firewall
-    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-    #Enable secure RDP authentication
-    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 1   
-}
-
-
-
-### Install the Certificate and Private Key (DSC requires it)
-Invoke-Command -ComputerName $vmName -Credential $adminCredential -ScriptBlock {
-    Import-PfxCertificate –FilePath D:\Certificates\craigweb.corp.microsoft.com.pfx -CertStoreLocation Cert:\LocalMachine\My -Password (ConvertTo-SecureString 'J$p1ter' -AsPlainText -Force)
-}
-
-
 
---
+++ BaseVM/New-FimVM.ps1
@@ -0,0 +1,93 @@
+﻿
+Set-Location $HOME
+
+$SwitchName       = Get-VMSwitch -SwitchType External | Select-Object -expand Name -First 1
+$ImageName        = "WindowsServer2012R2-Image"
+$ImageVhdFilePath = "S:\Hyper-V\Virtual Hard Disks\$ImageName.vhdx"
+$vmName           = "CraigmDev1118-5"
+$vhdFilePath      = "S:\Hyper-V\Virtual Hard Disks\$vmName.vhdx"
+$adminCredential  = New-Object System.Management.Automation.PSCredential $vmName\administrator,(ConvertTo-SecureString 'PA$$w0rd2014' -AsPlainText -Force)
+$isoFolderPath    = "S:\ISO"
+$StartUpMemoryMB  = 4096
+$ProcessorCount   = 4
+$UnattendFilePath = "$HOME\$vmName.xml"
+
+[xml]$UnattendFile = @'
+<?xml version='1.0' encoding='utf-8'?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+  <settings pass="specialize">
+    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <ComputerName>{0}</ComputerName>
+      <TimeZone>Pacific Standard Time</TimeZone>
+    </component>
+  </settings>
+  <settings pass="oobeSystem">
+    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+      <OOBE>
+        <HideEULAPage>true</HideEULAPage>
+        <SkipMachineOOBE>true</SkipMachineOOBE>
+        <SkipUserOOBE>true</SkipUserOOBE>
+        <ProtectYourPC>3</ProtectYourPC>
+        <NetworkLocation>Work</NetworkLocation>
+      </OOBE>
+      <UserAccounts>
+        <AdministratorPassword>
+          <Value>PA$$w0rd2014</Value>
+          <PlainText>true</PlainText>
+        </AdministratorPassword>
+      </UserAccounts>
+    </component>
+  </settings>
+</unattend>
+'@ -F $vmName
+
+$UnattendFile.Save($UnattendFilePath)
+
+Copy -Path $ImageVhdFilePath -Destination $vhdFilePath -Verbose -Force
+# del $vhdFilePath
+# dir $vhdfilePath
+
+### Mount the VHD
+Mount-WindowsImage -ImagePath $vhdFilePath -Index 1 -Path s:\Temp
+
+    ### Copy DSC Configuration into the VM
+    Copy -Path S:\Install\Test-FimOneBox -Destination 'S:\Temp\Temp' -Recurse
+
+    ### Copy the Unattend XML
+    Copy -Path $UnattendFilePath  -Destination S:\Temp\unattend.xml
+
+### Close and Save the VHD
+Dismount-WindowsImage -Path s:\Temp -Save 
+
+### Create the VM
+New-VM -Name $vmName -VHDPath $vhdFilePath -SwitchName $SwitchName -MemoryStartupBytes ($StartUpMemoryMB*1MB) -Generation 2 -BootDevice VHD 
+Set-VM -Name $vmName -ProcessorCount $ProcessorCount
+
+### Start the VM
+Start-VM -Name $vmName
+
+### Add a DVD Drive
+Add-VMDvdDrive -VMName $vmName
+
+### Mount the ISO with the FIM OneBox Files
+Set-VMDvdDrive -VMName $vmName -Path S:\ISO\FimOneBoxFiles.iso
+
+### Add the new VM to the TrustedHosts for this VM Host
+set-item WSMan:\localhost\Client\TrustedHosts –value $vmName -Force
+
+### Enable RDP
+Invoke-Command -ComputerName $vmName -Credential $adminCredential -ScriptBlock {
+    #Allow incoming RDP on firewall
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+    #Enable secure RDP authentication
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 1   
+}
+
+
+### Install the Certificate and Private Key (DSC requires it)
+Invoke-Command -ComputerName $vmName -Credential $adminCredential -ScriptBlock {
+    Import-PfxCertificate –FilePath D:\Certificates\craigweb.corp.microsoft.com.pfx -CertStoreLocation Cert:\LocalMachine\My -Password (ConvertTo-SecureString 'J$p1ter' -AsPlainText -Force)
+}
+
+
+
